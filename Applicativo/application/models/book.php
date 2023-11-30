6b9aa1e2637d68ade303e54eeb3adf87624fe120
LorenzonLuca <luca.lorenzon@samtrevano.ch>
2023-11-30 11:13:57 +0100

    fetch books OK + getAuthor & getPublisher

--- Applicativo/application/models/book.php
+++ Applicativo/application/models/book.php
@@ -18,6 +18,38 @@ class book
 
     }
     public static function fetchBooks(){
-        
+        $statement = 'SELECT * FROM book;';
+        $result = DB_CONNECTION->prepare($statement);
+        $result->execute();
+        $books = $result->fetch(PDO::FETCH_OBJ);
+
+        if (!$books) {
+            throw new Exception();
+        }
+        return books;
+    }
+    public static function getAuthor($id){
+        $statement = 'SELECT * FROM author WHERE id = :id;';
+        $result = DB_CONNECTION->prepare($statement);
+        $result->bindParam(':id', $id, PDO::PARAM_INT);
+        $result->execute();
+        $author = $result->fetch(PDO::FETCH_OBJ);
+
+        if (!$author) {
+            throw new Exception();
+        }
+        return $author;
+    }
+    public static function getPublisher($id){
+        $statement = 'SELECT * FROM publisher WHERE id = :id;';
+        $result = DB_CONNECTION->prepare($statement);
+        $result->bindParam(':id', $id, PDO::PARAM_INT);
+        $result->execute();
+        $publisher = $result->fetch(PDO::FETCH_OBJ);
+
+        if (!$publisher) {
+            throw new Exception();
+        }
+        return $publisher;
     }
 }
