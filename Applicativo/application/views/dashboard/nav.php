d2d8cc00c879d3928e4712f0710cc056038bc6b1
Jan Rezzonico <jan.rezzonico@samtrevano.ch>
2023-12-07 11:35:12 +0100

    Various changes

---
+++ Applicativo/application/views/dashboard/nav.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark px-2">
+    <a class="navbar-brand" href="#">Libreria</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <?php if ($_SESSION['is_admin']): ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="management">Gestione Libri</a>
+                </li>
+            <?php endif; ?>
+        </ul>
+    </div>
+    <div class="d-flex ms-auto">
+        <span class="navbar-text">
+            Welcome, <?php echo $_SESSION['username']; ?>
+        </span>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link px-1" href="<?php echo URL . 'login/logout' ?>">Logout</a>
+            </li>
+        </ul>
+    </div>
+</nav>
